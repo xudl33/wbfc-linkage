affa93e430c3364c3fe6d3d649d01f69f82eef3d
xudalin <xudl@wisea.com.cn>
2019-09-09 18:34:01 +0800

    增加测试url手动输入的功能； 修改默认端口号；

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,49 @@
 <template>
   <div id="app">
     <div class="wbfc-linkage block">
+    联动url:<el-input v-model="action" placeholder="http://192.168.20.5:8893/system/area/linkageList"></el-input><el-button type="primary" @click="reInit">点击重新初始化</el-button>
+    </div>
+    <div class="wbfc-linkage block">
       <span class="demonstration">直接使用v-model的绑定值</span>
-      <wbfc-linkage :url="action">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" ref="linkage0">
       </wbfc-linkage>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">初始化不加载</span>
-      <wbfc-linkage :url="action" :init="false" ref="linkageNotInit">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" :init="false" ref="linkage1">
       </wbfc-linkage>
       <el-button type="primary" @click="getData">点击加载数据</el-button>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">使用getValues函数</span>
-      <wbfc-linkage :url="action"v-model="selectedOptions2" ref="linkage2">
+      <wbfc-linkage v-if="isRouterAlive" :url="action"v-model="selectedOptions2" ref="linkage2">
       </wbfc-linkage>
       <el-button type="primary" @click="getData2">获取数值数据</el-button>
       <el-button type="primary" @click="getData3">获取第二级的值</el-button>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">设置默认值</span>
-      <wbfc-linkage :url="action" v-model="selectedOptions3" ref="linkage3">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" v-model="selectedOptions3" ref="linkage3">
       </wbfc-linkage>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">只联动2级</span>
-      <wbfc-linkage :url="action" ref="linkage4" :link-level="2">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" ref="linkage4" :link-level="2">
       </wbfc-linkage>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">选取任意节点</span>
-      <wbfc-linkage :url="action" ref="linkage5" :cascader-props="{checkStrictly: true}">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" ref="linkage5" :cascader-props="{checkStrictly: true}">
       </wbfc-linkage>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">多选节点</span>
-      <wbfc-linkage :url="action" ref="linkage6" :cascader-props="{multiple: true}">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" ref="linkage6" :cascader-props="{multiple: true}">
       </wbfc-linkage>
     </div>
     <div class="wbfc-linkage block">
       <span class="demonstration">多选节点默认值</span>
-      <wbfc-linkage :url="action" ref="linkage6" v-model="selectedOptions4" :cascader-props="{multiple: true}">
+      <wbfc-linkage v-if="isRouterAlive" :url="action" ref="linkage6" v-model="selectedOptions4" :cascader-props="{multiple: true}">
       </wbfc-linkage>
     </div>
   </div>
@@ -59,6 +62,7 @@ export default {
   },
   data () {
     return {
+      isRouterAlive: true,
       action: 'http://192.168.20.5:8893/system/area/linkageList',
       selectedOptions2:[],
       selectedOptions3: ['110000000000','110100000000','110101000000'],
@@ -70,8 +74,12 @@ export default {
     }
   },
   methods:{
+    reInit(){
+      this.isRouterAlive = false;
+      this.$nextTick(() => (this.isRouterAlive = true))
+    },
     getData() {
-      this.$refs.linkageNotInit.start();
+      this.$refs.linkage1.start();
     },
     getData2() {
       alert(this.$refs.linkage2.getValues());
